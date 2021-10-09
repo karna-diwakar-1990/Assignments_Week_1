0453143f88a3a13fb2ed93bb63d04abeac281c7d
karna diwakar <diwakarkarna1990@outlook.com>
2021-10-09 10:12:21 +0530

    Initial commit

---
+++ src/main/java/org/Test_Leaf/App.java
@@ -0,0 +1,13 @@
+package org.Test_Leaf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
